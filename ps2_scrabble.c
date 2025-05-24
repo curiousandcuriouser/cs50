c1de29637ea1e09d4437af55c784fc6be5a5eb48
curiousandcuriouser <204502050+curiousandcuriouser@users.noreply.github.com>
2025-05-24 19:18:57 +0200

    Refactor code by creating computeScore and declareWinner

--- ps2_scrabble.c
+++ ps2_scrabble.c
@@ -5,36 +5,29 @@
 #include <string.h>
 
 int computeScore(string word);
+void declareWinner(int score1, int score2);
 int points[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
 int main (void)
 {
-  
   // GET WORDS
-  string word1 = get_string("Player 1. Enter a word: \n");
-  string word2 = get_string("Player 2. Enter a word: \n");
+  string word1 = get_string("Player 1. Enter a word: ");
+  string word2 = get_string("Player 2. Enter a word: ");
 
-  // GET SCORE
-  int score1 = computeScore(word1);
-  printf("Player 1 scores %i points\n", score1);
-  int score2 = computeScore(word2);
-  printf("Player 1 scores %i points\n", score2);
+  //GET SCORE
+  int score1 =  computeScore(word1);
+  int score2 =  computeScore(word2);
 
-  // DECLARE WINNER
-  if (score1 > score2)
-  {
-    printf("Player 1 wins!\n");
-  }
-  else if (score2 > score1)
-  {
-    printf("Player 2 wins!\n");
-  }
-  else
-  {
-    printf("Tie\n");
-  }
+  //PRINT SCORE
+  printf("Player 1 Score: %i\n", score1);
+  printf("Player 2 Score: %i\n", score2);
+  
+  //DECLARE WINNER
+  declareWinner(score1, score2);
+  
 }
 
+// COMPUTE SCORE
 int computeScore(string word)
 {
   int score = 0;
@@ -56,13 +49,19 @@ int computeScore(string word)
   return score;
 }
 
-/*
-1. Ask for input person 1
-2. Ask for input person 2
-ADD VALIDATION THAT IS IS NOT A NUMBER
-
-
-3. compare inputs
-4. decide winner
-
-*/
+// DECLARE WINNER
+void declareWinner(int score1, int score2)
+{
+  if (score1 > score2)
+  {
+    printf("Player 1 wins!\n");
+  }
+  else if (score2 > score1)
+  {
+    printf("Player 2 wins!\n");
+  }
+  else
+  {
+    printf("Tie\n");
+  }
+}
