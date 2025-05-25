6aad58cb1a54b1deb015eee450bcc43873facc9a
curiousandcuriouser <204502050+curiousandcuriouser@users.noreply.github.com>
2025-05-25 20:06:14 +0200

    Create encryption with formula

--- ps2_caesar.c
+++ ps2_caesar.c
@@ -1,5 +1,7 @@
 #include "cs50.h"
+#include <ctype.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
@@ -16,45 +18,33 @@ int main(void)
   // ASK FOR TEXT
   string plaintext = get_string("Enter your text:  ");
 
-  // MAKE CIPHERTEXT
-  char uppercaseLetters[26] = {65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90}; // A - Z
-
-  char lowercaseLetters[26] = {97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122}; // a - z
-
+  // MAKE & OUTPUT CIPHERTEXT
   for (int i = 0, length = strlen(plaintext); i < length; i++)
-    if (plaintext[i] >= 65 && plaintext[i] <= 90)
+  {
+    char currentCharacter = plaintext[i];
+
+    if (isupper(currentCharacter))
     {
-      plaintext[i] += key;
+      char encryptedCharacter = ((currentCharacter - 'A') + key) % 26 + 'A';
+      printf("%c", encryptedCharacter);
     }
-    else if (plaintext[i] >= 97 && plaintext[i] <= 122)
+    else if (islower(currentCharacter))
     {
-      plaintext[i] += key;
+      char encryptedCharacter = ((currentCharacter - 'a') + key) % 26 + 'a';
+      printf("%c", encryptedCharacter);
     }
     else
     {
-
+      printf("%c", currentCharacter);
     }
-
-
-  // OUTPUT CIPHERTEXT
+  }
+  printf("\n");
 
   
   return 0;
 }
 
 /*
-/ Ask user to int
-  / Int must be only one and positive
-  / If user submits more than one or none, print error message and return 1
-  If user does not submit decimal digit, print Usage: ./caesar key and return 1
-/ Then ask for input plaintext: space space and ask for a string
-3. Then output ciphertext: space
-/ Return 0 after successful execution
-
-
-General:
-- Mustr preserve case
-- If not a letter, maintain
-- 
-
+TODO:
+1. Make reverse
 */
