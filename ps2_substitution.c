8822d68d3563ee4074be0b46ac3b2ec1cdca151d
curiousandcuriouser <204502050+curiousandcuriouser@users.noreply.github.com>
2025-05-25 20:29:22 +0200

    Create loop to verify key and get_stringto ask for text

--- ps2_substitution.c
+++ ps2_substitution.c
@@ -1,9 +1,34 @@
 #include "cs50.h"
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
   // ASK FOR SUBSITUTION KEY
   string substitutionKey = get_string("Enter the substitution key: \n");
+
+  // VERIFY SUBSTITUTION KEY
+  for (int i = 0, length = strlen(substitutionKey); i < length; i++)
+  {
+    if (!isalpha(substitutionKey[i]))
+    {
+        printf("Your input has to consist of only letters.\n");
+        return 1;
+    }
+  }
+
+  if (strlen(substitutionKey) < 26)
+    {
+        printf("Your input has to consist of 26 letters.\n");
+        return 1;
+    }
+
+  // ASK FOR TEXT
+  string plaintext = get_string("Enter your text: \n");
+
+  // ENCRYPT AND OUTPUT CIPHERTEXT
+  
 }
 
 /*
