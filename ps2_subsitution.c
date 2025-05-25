5e9e5eedf9a6f2210243e206a281bcad0db1e413
curiousandcuriouser <204502050+curiousandcuriouser@users.noreply.github.com>
2025-05-25 20:13:04 +0200

    Pseudocode

---
+++ ps2_subsitution.c
@@ -0,0 +1,19 @@
+#include "cs50.h"
+
+int main(void)
+{
+
+}
+
+/*
+1. Ask for substitution key
+  If no or too many CLA, print error message and return 1
+  If invalid, print error message and return 1
+2. Ask for plaintext with get_string
+3. Output ciphertext
+4. Print new line and return 0
+
+General:
+- Key should be case-insensitive
+- Preserve text case
+*/
