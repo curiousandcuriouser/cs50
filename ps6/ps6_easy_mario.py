ce7fcb05b5f89368bc72c3e27fbba7970dd60fc8
curiousandcuriouser <204502050+curiousandcuriouser@users.noreply.github.com>
2025-06-01 14:34:49 +0200

    Add validation input 1-8

--- ps6/ps6_easy_mario.py
+++ ps6/ps6_easy_mario.py
@@ -1,5 +1,12 @@
-height = int(input("How high should the pyramid be? "))
+height = input("How high should the pyramid be? ")
 
+try: 
+  height = int(height)
+except ValueError: 
+  height = input("How high should the pyramid be? ")
+
+while height < 1 or height > 8:
+  height = int(input("How high should the pyramid be? ")) 
 
 i = 1
 while i <= height:
