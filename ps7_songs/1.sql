442638390fbcb63445f05cf02770af63f330d161
curiousandcuriouser <204502050+curiousandcuriouser@users.noreply.github.com>
2025-06-03 10:09:20 +0200

    List all song names ordered in descending tempo

--- ps7_songs/1.sql
+++ ps7_songs/1.sql
@@ -2,3 +2,4 @@ sqlite3 songs.db
 .headers on
 .mode table
 SELECT name FROM songs; -- Makes one column with all the song names
+SELECT name FROM songs ORDER BY tempo DESC; -- List all song names ordered in descending tempo
