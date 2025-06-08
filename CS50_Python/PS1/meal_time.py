4be90b6462b06d6ee5316749a0f768411c0454c6
curiousandcuriouser <204502050+curiousandcuriouser@users.noreply.github.com>
2025-06-09 00:15:30 +0200

    Add convert(time) and conditional for evaluation

---
+++ CS50_Python/PS1/meal_time.py
@@ -0,0 +1,21 @@
+def main():
+  time = input("What time it is? ")
+  time = convert(time)
+
+  if 7.0 <= time <= 8.0:
+    print("breakfast time")
+  elif 12.0 <= time <= 13.0:
+    print("breakfast time")
+  elif 18.0 <= time <= 19.0:
+    print("dinner time")
+  else:
+    return
+
+def convert(time):
+  hour = float(time.split(":")[0])
+  minutes = (float(time.split(":")[1])) * float(1/60)
+  time = float(hour + minutes)
+  return time
+
+  
+main()
