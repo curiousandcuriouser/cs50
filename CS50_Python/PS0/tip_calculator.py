55aa64475eb8787f97ef43a9f06f1ac1c8ecfec1
curiousandcuriouser <204502050+curiousandcuriouser@users.noreply.github.com>
2025-06-08 20:44:13 +0200

    Assign modified variable to variable

--- CS50_Python/PS0/tip_calculator.py
+++ CS50_Python/PS0/tip_calculator.py
@@ -6,12 +6,13 @@ def main():
 
 
 def dollars_to_float(d):
-  d.replace("$", "")
+  d = d.replace("$", "")
   return float(d)
 
 
 def percent_to_float(p):
-  
+  p = p.replace("%", "")
+  return float(int(p) / 100)
 
 
 main()
