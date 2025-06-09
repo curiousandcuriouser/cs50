5acf2dc07c83c8d793a27c1ec572907f76f1a516
curiousandcuriouser <204502050+curiousandcuriouser@users.noreply.github.com>
2025-06-09 12:36:17 +0200

    Make while loop validating input and calculating amount due

--- CS50_Python/PS2/coke_machine.py
+++ CS50_Python/PS2/coke_machine.py
@@ -3,7 +3,14 @@
 
 amount_due = 50
 
- 
+while amount_due > 0:
+  coin = int(input("Insert Coin: "))
+  if coin == 25 or coin == 10 or coin == 5:
+    amount_due -= coin
+  else: 
+    pass
+
+   
 
 
 # Display amount due
