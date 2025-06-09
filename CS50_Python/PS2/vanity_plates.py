e3787f1d1bc811fb9fcf628cbb975e30117d0bad
curiousandcuriouser <204502050+curiousandcuriouser@users.noreply.github.com>
2025-06-09 21:00:27 +0200

    Remove prints and add explanatory comments

--- CS50_Python/PS2/vanity_plates.py
+++ CS50_Python/PS2/vanity_plates.py
@@ -16,32 +16,37 @@ def main():
       print("Invalid")
 
 def is_valid(plate):
+  # Validate plate char length
   if 2 > len(plate) or len(plate) > 6:
-    print("Failed at: 2 > len(character_list) > 6")
     return False
   
+  # Validate first 2 characters letters
   if not plate[0].isalpha() or not plate[1].isalpha():
-    print("Failed at: first 2 digits")
     return False
 
   in_number = False
   
+  
   for i in range(len(plate)):
     character = plate[i]
+
+    # Validate all plate char digits and letters
     if not character.isalnum():
-      print("Failed at: all numbers & letters")
       return False
   
+
     if character.isdigit():
+      # Check if first number is 0
       if not in_number:
         if character == '0':
-          print("Failed at: first number is 0")
           return False
         in_number = True
+
+    # Check if letter after number
     elif character.isalpha():
       if in_number:
-        print("Failed at: letter after a number (number in middle)")
         return False
+  # If not false, then valid
   return True
 
 main()
