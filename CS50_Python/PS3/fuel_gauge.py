505b8b590caccabb7c283a43a3814f315344dced
curiousandcuriouser <204502050+curiousandcuriouser@users.noreply.github.com>
2025-06-09 22:54:32 +0200

    Remove print to remove None

--- CS50_Python/PS3/fuel_gauge.py
+++ CS50_Python/PS3/fuel_gauge.py
@@ -1,27 +1,28 @@
-# Prompt for input: X/Y
-while True:
-  try:
-    fraction = input("Provide a fraction in this format, X / Y: ")
-    
-    # Split numbers and calculate rounded percentage
-    numbers = fraction.split("/")
+def main():
+  x = display_fuel("Provide a fraction in this format, X / Y: ")
+  print(x)
 
-    if numbers[0] > numbers[1]:
-      continue
+def display_fuel(prompt):
+  # Prompt for input: X/Y
+  while True:
+    try:
+      x, y = input(prompt).split("/")
+      if x > y:
+        continue
+      percentage = round((int(x) / int(y))*100)
 
-    percentage = round((int(numbers[0]) / int(numbers[1]))*100)
+      # Display take gauge:
+      if 1 < percentage < 99:
+        return f"{percentage}%"
+      elif percentage <= 1:
+        return "E"
+      else:
+        return "F"
 
-    # Display take gauge:
-    if 1 < percentage < 99:
-      print(f"{percentage}%")
-    elif percentage <= 1:
-      print("E")
-    else:
-      print("F")
+    except (ValueError, ZeroDivisionError):
+      return
 
-    break
-  except (ValueError, ZeroDivisionError):
-    pass
+main()
 
 
 
