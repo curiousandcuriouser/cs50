3efeefd4f484247f69e69ddac7eee804aa28a661
curiousandcuriouser <204502050+curiousandcuriouser@users.noreply.github.com>
2025-06-09 23:14:21 +0200

    Add instructions

---
+++ CS50_Python/PS3/grocery_list.py
@@ -0,0 +1,7 @@
+# Prompt  user for items, one per line
+  # Treat the user’s input case-insensitively
+# Stop if input control-d
+# Output the user’s grocery list
+  # in all uppercase
+  # sorted alphabetically
+  # prefixing each line with the number of times the user inputted that item. No need to pluralize the items. 
