1325deb0403de6c6d5e41f1f4d08b0c37b15b692
curiousandcuriouser <204502050+curiousandcuriouser@users.noreply.github.com>
2025-06-09 23:29:33 +0200

    Add sorted

--- CS50_Python/PS3/grocery_list.py
+++ CS50_Python/PS3/grocery_list.py
@@ -1,20 +1,31 @@
-shopping_list = []
+def main():
+  get_item()
+  display_list()
 
-# Prompt  user for items, one per line
-while True:
-  try:
-    # Ask for input and convert to UPPERCASE
-    item = input("Item: ").upper()
-    shopping_list.append(item)
-      
-  # Stop when use input Ctrl + D
-  except EOFError:
-    print("\n")
-    break
-  else:
-    pass
 
+def get_item():
+  shopping_list = []
+
+  # Prompt  user for items, one per line
+  while True:
+    try:
+      # Ask for input and convert to UPPERCASE
+      item = input("Item: ").upper()
+      shopping_list.append(item)
+      print(f"{shopping_list}")
+    
+
+        
+    # Stop when use input Ctrl + D
+    except EOFError:
+      return shopping_list
+
+
+def display_list():
 # Output the user’s grocery list
   # in all uppercase
   # sorted alphabetically
   # prefixing each line with the number of times the user inputted that item. No need to pluralize the items. 
+  sorted(shopping_list)
+
+main()
