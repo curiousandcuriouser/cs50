76ed368ec6099ef43866d95180e30c49a628f7c8
curiousandcuriouser <204502050+curiousandcuriouser@users.noreply.github.com>
2025-06-09 22:37:01 +0200

    Make conditional to disallow 100+%

--- CS50_Python/PS3/fuel_gauge.py
+++ CS50_Python/PS3/fuel_gauge.py
@@ -2,21 +2,28 @@
 while True:
   try:
     fraction = input("Provide a fraction in this format, X / Y: ")
+    
+    # Split numbers and calculate rounded percentage
+    numbers = fraction.split("/")
+
+    if numbers[0] > numbers[1]:
+      continue
+
+    percentage = round((int(numbers[0]) / int(numbers[1]))*100)
+
+    # Display take gauge:
+    if 1 < percentage < 99:
+      print(f"{percentage}%")
+    elif percentage <= 1:
+      print("E")
+    else:
+      print("F")
+
     break
   except (ValueError, ZeroDivisionError):
     pass
 
-# Split numbers and calculate rounded percentage
-numbers = fraction.split("/")
-percentage = round((int(numbers[0]) / int(numbers[1]))*100)
 
-# Display take gauge:
-if 1 < percentage < 99:
-  print(f"{percentage}%")
-elif percentage <= 1:
-  print("E")
-else:
-  print("F")
 
 
 
