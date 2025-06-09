ea235f9b3404d17362b30388ae30273b63fea01a
curiousandcuriouser <204502050+curiousandcuriouser@users.noreply.github.com>
2025-06-09 22:12:29 +0200

    Ask for fraction input

---
+++ CS50_Python/PS3/fuel_gauge.py
@@ -0,0 +1,9 @@
+# Prompt for input: X/Y
+fraction = "Provide a fraction in this format, X/Y: "
+
+# Convert to percentage
+# Round to nearest int
+# Display take gauge:
+  # 1 => E
+  # 99 =< F
+  # Others in percent
