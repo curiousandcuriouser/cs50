0a5bbdccd3294de6b63e822d00eef9ff9a007dd1
curiousandcuriouser <204502050+curiousandcuriouser@users.noreply.github.com>
2025-06-09 12:35:45 +0200

    Initialize amount_due

---
+++ CS50_Python/PS2/coke_machine.py
@@ -0,0 +1,11 @@
+# 1 bottle costs 50c
+# The machine only accepts these denominations: 25 cents, 10 cents, and 5 cents
+
+amount_due = 50
+
+ 
+
+
+# Display amount due
+# If > 0, prompt again
+# Caluclate change owed in case user added too much
