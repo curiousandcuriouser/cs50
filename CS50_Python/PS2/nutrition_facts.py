29d0cf5a7687afc6875d73acac1ce1a42a801f65
curiousandcuriouser <204502050+curiousandcuriouser@users.noreply.github.com>
2025-06-09 21:13:38 +0200

    Create dictionary of fruit and cals

--- CS50_Python/PS2/nutrition_facts.py
+++ CS50_Python/PS2/nutrition_facts.py
@@ -2,6 +2,31 @@
 fruit = input("Fruit: ")
 
 # Make input case insensitive
-fruit = fruit.tolower()
+fruit = fruit.lower()
+
+# Make dict for fruit info
+fruits = [
+  {"fruit": "apple", "calories": "130" }
+  {"fruit": "avocado", "calories": "50"}
+  {"fruit": "banana", "calories": "110"}
+  {"fruit": "cantaloupe", "calories": "50"}
+  {"fruit": "grapefruit", "calories": "60"}
+  {"fruit": "grapes", "calories": "90"}
+  {"fruit": "honeydew melon", "calories": "50"}
+  {"fruit": "kiwi", "calories": "90"}
+  {"fruit": "lemon", "calories": "15"}
+  {"fruit": "lime", "calories": "20"}
+  {"fruit": "nectarine", "calories": "60"}
+  {"fruit": "orange", "calories": "80"}
+  {"fruit": "peach", "calories": "60"}
+  {"fruit": "pear", "calories": "100"}
+  {"fruit": "pineapple", "calories": "50"}
+  {"fruit": "plums", "calories": "70"}
+  {"fruit": "strawberries", "calories": "50"}
+  {"fruit": "cherries", "calories": "100"}
+  {"fruit": "tangerine", "calories": "50"}
+  {"fruit": "watermelon", "calories": "80"}
+]
+
 # Output calories for one portion
 
