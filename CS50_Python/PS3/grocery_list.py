8e5d3c19dc67864a097598fcd56e5add5224daad
curiousandcuriouser <204502050+curiousandcuriouser@users.noreply.github.com>
2025-06-10 09:49:33 +0200

    Import Counter to output sorted grocery list with count

--- CS50_Python/PS3/grocery_list.py
+++ CS50_Python/PS3/grocery_list.py
@@ -1,31 +1,27 @@
 def main():
-  get_item()
-  display_list()
+  shopping_list = get_item()
+  display_list(shopping_list)
 
-
+# Prompt  user for items
 def get_item():
   shopping_list = []
-
-  # Prompt  user for items, one per line
   while True:
     try:
       # Ask for input and convert to UPPERCASE
       item = input("Item: ").upper()
       shopping_list.append(item)
-      print(f"{shopping_list}")
-    
-
         
     # Stop when use input Ctrl + D
     except EOFError:
       return shopping_list
 
 
-def display_list():
-# Output the user’s grocery list
-  # in all uppercase
-  # sorted alphabetically
-  # prefixing each line with the number of times the user inputted that item. No need to pluralize the items. 
-  sorted(shopping_list)
+# Output the user’s sorted grocery list with count
+def display_list(shopping_list):
+  from collections import Counter
+  item_count = Counter(shopping_list)
+  print("Shopping List:")
+  for item, item_count in sorted(item_count.items()):
+    print(f"{item_count} {item}")
 
 main()
