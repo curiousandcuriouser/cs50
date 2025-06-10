0d6bc341c5bc9d4e7b74d85acb485f83d79f9c59
curiousandcuriouser <204502050+curiousandcuriouser@users.noreply.github.com>
2025-06-10 18:35:24 +0200

    Add adieu to format names

--- CS50_Python/PS4/adieu_adieu.py
+++ CS50_Python/PS4/adieu_adieu.py
@@ -16,4 +16,22 @@ def get_names():
       print()
       return names
 
+
+def adieu(names):
+  if len(names) == 1:
+    print(f"Adieu, adieu, to {names[0]}.")
+  elif len(names) == 2:
+    print(f"Adieu, adieu, to {names[0]} and {names[1]}.")
+  else:
+    print("Adieu, adieu, to ", end="")
+    i = len(names)
+    j = 0
+    while i > 1:
+      print(f"{names[j]}, ", end="")
+      i -= 1
+      j += 1
+    if i == 1:
+      print(f"and {names[j]}.")
+      i -= 1
+
 main()
