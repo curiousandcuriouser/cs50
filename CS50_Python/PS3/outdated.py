94dc8cfcbeb8fad8a0c0d86d134e6f0694054f06
curiousandcuriouser <204502050+curiousandcuriouser@users.noreply.github.com>
2025-06-10 10:42:43 +0200

    Complete convert_date() covering alphanumeric month cases

--- CS50_Python/PS3/outdated.py
+++ CS50_Python/PS3/outdated.py
@@ -20,20 +20,45 @@ def main ():
 def get_date():
   while True:
     date = input("Date in month-day-year format: ")
-    date.split("/")
-    date.split(" ", ",")
 
-    if not date[0] in months:
+    if "/" in date:
+      date = date.split("/")
+      if not 0 < int(date[0]) < 13:
+        print("Error months num")
+        continue
+
+    elif "," in date:
+      date = date.replace(",", "")
+      date = date.split(" ")
+      if not date[0] in months:
+        print("Error months word")
+        continue
+    else:
+      print("Split Issue")
+      return
+    
+    
+    if not int(date[2]) >= 0:
+      print("Error year")
       continue
-    if not 0 < date[1] < 13:
-      continue
-    if not date[3] > 0:
+    elif not 0 < int(date[1]) < 32:
+      print("Error day")
       continue
     else: 
       return date
 
-def convert_date():
-  print("\n")
+def convert_date(date):
+  day = int(date[1])
+  year = date[2]
+  month = date[0]
+
+  if month.isalpha():
+    # Find if  month is in list, convert, and print
+    if month in months:
+      month = months[month]
+      print(f"{day:02}/{month:02}/{year}")
+  elif month.isdigit():
+    print(f"{day:02}/{month:02}/{year}")
 
 main()
 
