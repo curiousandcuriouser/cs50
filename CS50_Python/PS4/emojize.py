af257ff49ee11ed9bc12c489df4282f184268ecf
curiousandcuriouser <204502050+curiousandcuriouser@users.noreply.github.com>
2025-06-10 16:43:59 +0200

    Emojize input

--- CS50_Python/PS4/emojize.py
+++ CS50_Python/PS4/emojize.py
@@ -1,8 +1,9 @@
 import emoji
 
 # Ask user for str
-  # Validate if starts & ends with :
-  # Remove :
-  #
+text = input("Enter the emoji you want in this format: :wave: ")
+
 # Output emoji
+print(emoji.emojize(text))
+
 
