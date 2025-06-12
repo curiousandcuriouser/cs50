0b0f60abe2b4ad2557f6e0ef9b1f6301037c7ba0
curiousandcuriouser <204502050+curiousandcuriouser@users.noreply.github.com>
2025-06-12 11:31:58 +0200

    Make 10 calculations and evaluate score

--- CS50_Python/PS4/little_professor.py
+++ CS50_Python/PS4/little_professor.py
@@ -4,8 +4,6 @@ import sys
 def main():
   level = get_level()
   generate_integer(level)
-  get_answers()
-  display_score()
 
 
 # Prompts the user for a level
@@ -26,40 +24,45 @@ def get_level():
 
 # Randomly generates ten (10) math problems formatted as X + Y = , wherein each of X and Y is a non-negative integer with n digits. No need to support operations other than addition (+).
 def generate_integer(level):
-  if level == 1:
-    int1 = random.randint(0, 9)
-    int2 = random.randint(0, 9)
-  elif level == 2:
-    int1 = random.randint(0, 99)
-    int2 = random.randint(0, 99)
-  else:
-    int1 = random.randint(0, 999)
-    int2 = random.randint(0, 999)
-
   score = 0
-  attempt = 0
   repetition = 0
 
-  while repetition < 11:
-    answer = input(f"{int1} + {int2} = ")
-    sum = int1 + int2
-    print(sum)
+  while repetition < 10:
+    if level == 1:
+      int1 = random.randint(0, 9)
+      int2 = random.randint(0, 9)
+    elif level == 2:
+      int1 = random.randint(0, 99)
+      int2 = random.randint(0, 99)
+    else:
+      int1 = random.randint(0, 999)
+      int2 = random.randint(0, 999)
+    
+    attempt = 0
+    correct_sum = int1 + int2 # works
 
-    while attempt < 4:
-      if answer == sum:
+    while attempt < 3:
+      while True:
+        try:
+          answer = int(input(f"{int1} + {int2} = "))
+          break
+        except ValueError:
+          attempt += 1
+          print("EEE")
+          continue
+
+      if answer == correct_sum:
         score += 1
-      elif answer != sum:
+        repetition += 1
+        break
+      elif answer != correct_sum:
         attempt += 1
         print("EEE")
-    print(score)
+        if attempt == 3:
+          repetition += 1
+          break
 
-#Prompts the user to solve each of those problems. If an answer is not correct (or not even a number), the program should output EEE and prompt the user again, allowing the user up to three tries in total for that problem. If the user has still not answered correctly after three tries, the program should output the correct answer.
-def get_answers():
-    ...
-
-# The program should ultimately output the user’s score: the number of correct answers out of 10.
-def display_score():
-    ...
+  print(f"Score: {score}")
 
 if __name__ == "__main__":
     main()
