bcbeb09a86dc90673c2d337f14009f16fd258718
curiousandcuriouser <204502050+curiousandcuriouser@users.noreply.github.com>
2025-06-12 17:44:27 +0200

    Add tabulate

--- CS50_Python/PS6/pizza_py.py
+++ CS50_Python/PS6/pizza_py.py
@@ -13,12 +13,11 @@ try:
     print("Not a CSV file")
     sys.exit()
 
-  with open(sys.argv[1]) as file:
-    lines = file.readlines()
+  # Output a table formatted as ASCII art using tabulate
+  print(tabulate(sys.argv[1], headers="firstrow", tablefmt="grid")) 
 
 except FileNotFoundError:
     print("File does not exist")
     sys.exit()
 
-# Output a table formatted as ASCII art using tabulate
-print(tabulate(sys.argv[1], headers="firstrow", tablefmt="grid"))
+
