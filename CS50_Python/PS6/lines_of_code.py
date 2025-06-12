4371e5b9e7420908f335c24c9956f07c3fbeb371
curiousandcuriouser <204502050+curiousandcuriouser@users.noreply.github.com>
2025-06-12 17:11:07 +0200

    Add argument validator

---
+++ CS50_Python/PS6/lines_of_code.py
@@ -0,0 +1,35 @@
+import sys
+
+# If the user does not specify exactly one command-line argument, or file not ending .py, or file not exist, exit via sys.exit.
+try:
+  if len(sys.argv) < 2:
+    print("Too few command-line arguments")
+    sys.exit()
+  elif len(sys.argv) > 2:
+    print("Too many command-line arguments")
+    sys.exit()
+  elif not sys.argv[1].endswith(".py"):
+    print("Not a Python file")
+    sys.exit()
+
+  with open(sys.argv[1]) as file:
+    lines = file.readlines()
+
+except FileNotFoundError:
+    print("File does not exist")
+    sys.exit()
+
+
+
+count = 0
+
+for line in lines:
+  if line.startswith(" "):
+    pass
+  elif line.startswith("#"):
+    pass
+  else:
+    count += 1
+
+#Outputs the number of lines of code in that file, excluding comments and blank lines
+print(count)
