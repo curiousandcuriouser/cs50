79591e66b639e06495656540a9b628a527b33ba4
curiousandcuriouser <204502050+curiousandcuriouser@users.noreply.github.com>
2025-06-16 10:55:30 +0200

    Add correct validate function

--- CS50_Python/PS7/numb3rs.py
+++ CS50_Python/PS7/numb3rs.py
@@ -5,13 +5,12 @@ def main():
   print(validate(input("IPv4 Address: ")))
 
 def validate(ip):
-  if re.search(r"^[0-255]\.[0-255]\.[0-255]\.[0-255]$", ip):
+  range = r"(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]|[0-9])"
+  pattern = rf"^{range}\.{range}\.{range}\.{range}$"
+  if re.search(pattern, ip):
     return True
   else:
     return False
 
 if __name__ == "__main__":
   main()
-
-  #An IPv4 address is a numeric identifier that a device (or, on TV, hacker) uses to communicate on the internet, akin to a postal address in the real world, typically formatted in dot-decimal notation as #.#.#.#. But each # should be a number between 0 and 255, inclusive. Suffice it to say 275 is not in that range!
-  # implement a function called validate that expects an IPv4 address as input as a str and then returns True or False, respectively, if that input is a valid IPv4 address or not.
