9e2b994d9649dff272a7fa84569acbd445957d60
curiousandcuriouser <204502050+curiousandcuriouser@users.noreply.github.com>
2025-06-16 11:57:44 +0200

    Add link regex and formatting

--- CS50_Python/PS7/tests/test_watch_on_youtube.py
+++ CS50_Python/PS7/tests/test_watch_on_youtube.py
@@ -1,7 +1,7 @@
 from PS7.watch_on_youtube import parse
 
 def test_parse_short():
-  assert parse('<iframe src="http://www.youtube.com/embed/xvFZjo5PgG0"></iframe>') == "https://youtu.be/xvFZjo5PgG0"
+  assert parse('<iframe src="https://www.youtube.com/embed/xvFZjo5PgG0"></iframe>') == "https://youtu.be/xvFZjo5PgG0"
 
 def test_parse_long():
   assert parse('<iframe width="560" height="315" src="https://www.youtube.com/embed/xvFZjo5PgG0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>') == "https://youtu.be/xvFZjo5PgG0"
--- CS50_Python/PS7/watch_on_youtube.py
+++ CS50_Python/PS7/watch_on_youtube.py
@@ -5,10 +5,13 @@ import sys
 def main():
     print(parse(input("HTML: ")))
 
-
 def parse(s):
-  if re.search("youtube", s):
-    return True
+  if match := re.search(r"src=[\"'](https?:\/\/(?:www\.)?youtube\.com\/embed\/[^\"']+)[\"']", s):
+    full_link = match.group(1)
+    formatted_link = full_link.replace("www.youtube.com/embed", "youtu.be")
+    return formatted_link
+
+  
   else:
     return f"None"
     # Find the correct section
